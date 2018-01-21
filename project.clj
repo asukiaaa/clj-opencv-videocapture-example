55bf1f5b4c1c29626555e196849e63aa6d535dd5
asuki <asukiaaa@gmail.com>
2018-01-21 10:49:13 +0900

    Update version of dependencies

--- project.clj
+++ project.clj
@@ -3,9 +3,9 @@
   :url "http://example.com/FIXME"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[clj-time "0.14.0"]
-                 [org.clojure/clojure "1.8.0"]
-                 [org.clojure/core.async "0.3.442"]]
+  :dependencies [[clj-time "0.14.2"]
+                 [org.clojure/clojure "1.9.0"]
+                 [org.clojure/core.async "0.4.474"]]
   :jvm-opts ["-Djava.library.path=lib"]
   :resource-paths ["lib/opencv-340.jar"]
   :injections [(clojure.lang.RT/loadLibrary org.opencv.core.Core/NATIVE_LIBRARY_NAME)]
